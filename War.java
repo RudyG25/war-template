844a8f0ad27ff7ec001b8acefd444ae1a1ce8f1c
Rodolfo Good <rodolfo.good@mrjaffesclass.com>
2023-01-05 12:46:39 -0800

    got a good amount of event loop done

--- War.java
+++ War.java
@@ -40,9 +40,67 @@ public class War
      * from the War flowchart you created for this game
      */
     public void runEventLoop(Deck plA, Deck plB) {
-                
+        Card A;
+        Card B;
+        System.out.println("War:");
+        int turn = 1;
+        while (plA.getDeckSize() > 0 && plA.getDeckSize() > 0 && turn <= 300) {
+            Deck storage = new Deck();
+            System.out.println("It is turn " + turn + ".");
+             System.out.println("Player A has " + plA.getDeckSize() + 
+            " and player B has " + plB.getDeckSize() + " cards left.");
+            A = plA.dealCardFromDeck();
+            B = plB.dealCardFromDeck();
+            System.out.println("Player A drew a " + A.getFace() + " of " +  
+            A.getSuit());
+            System.out.println("and Player B drew a " + B.getFace() + " of " + 
+            B.getSuit());
+            turnWinner(plA, plB, A, B, storage);
+            turn++;
+        }
+        
     }
-    
+    public void war(Deck plA, Deck plB, Card A, Card B) {
+        if (plA.getDeckSize() >= 4 && plB.getDeckSize() >= 4) {
+            System.out.println("A and B both drew the same card! time for war.");
+            Deck storage = new Deck();
+            storage.addCardToDeck(A);
+            storage.addCardToDeck(B);
+            for (int i = 0; i < 3; i++) {
+                storage.addCardToDeck(plA.dealCardFromDeck());
+                
+            }
+            for (int i = 0; i < 3; i++) {
+                storage.addCardToDeck(plB.dealCardFromDeck());
+                
+            }
+            Card A2 = plA.dealCardFromDeck();
+            Card B2 = plB.dealCardFromDeck();
+            turnWinner(plA, plB, A2, B2, storage);
+        }
+    }
+    public void turnWinner (Deck plA, Deck plB, Card A, Card B, Deck storage) {
+        if (A.getRank() == B.getRank()) {
+                war(plA, plB, A, B);
+                
+            }
+            else if (A.getRank() > B.getRank()) {
+                System.out.println("A's card is higher than B's card, so A wins");
+                plA.addCardToDeck(A);
+                plA.addCardToDeck(B);
+                for (int i = 0; i < storage.getDeckSize(); i++) {
+                    plA.addCardToDeck(storage.dealCardFromDeck());
+                }
+            }
+            else {
+                System.out.println("B's card is higher than A's card, so B wins");
+                plB.addCardToDeck(B);
+                plB.addCardToDeck(A);
+                for (int i = 0; i < storage.getDeckSize(); i++) {
+                    plB.addCardToDeck(storage.dealCardFromDeck());
+                }
+            }
+        }
     /**
      * The main method is called when Java starts your program
      */
